2e68844b36fe27837d5ffa2f70447fff6d90dcda
Evanda Remington <eremington@manh.com>
2017-04-20 11:38:38 -0400

    A little more detail rendered from the thread json (notably locks and thread state)

--- src/app/thread.ts
+++ src/app/thread.ts
@@ -2,5 +2,19 @@ import {Trace} from "./trace";
 export class Thread {
   threadName: string;
   threadId: number;
+  threadState: string;
+
+  lockName: string;
+  lockOwnerId: number;
+  lockOwnerName: string;
+
+  blockedTime: number;
+  blockedCount: number;
+  waitedTime: number;
+  waitedCount: number;
+
+  isNative: boolean;
+  suspended: boolean;
+
   stackTrace: Trace[];
 }
